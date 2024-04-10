2190fe7156d3d3b194aeb4887c96b7c7d936c762
roberto <rvillegas@outlook.com>
2024-04-09 22:56:22 -0400

    fin seccion 8

---
+++ 8 Alias.sql
@@ -0,0 +1,13 @@
+--- sección 8
+--- Alias
+-- se utiliza AS para asignar un nombre temporal a una columna o tabla
+-- útil en joins y en funciones complejas
+
+select customer_id  as "serial_number", customer_name as "name", age as "customer_age" from customer;
+
+
+--fin sección 8 
+
+
+
+
