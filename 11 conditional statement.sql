c1989c63051d6c240e2d3fedffba356b942d2eb3
roberto <rvillegas@outlook.com>
2024-04-12 21:23:34 -0400

    fin sección 11

--- 11 conditional statement.sql
+++ 11 conditional statement.sql
@@ -3,9 +3,21 @@
 
 -- 45 Case When:
 
+--ejemplo
+Select * , Case 
+                when age <30 then 'Young'
+                when age>60 then 'Senior Citizen'
+                else 'Middle Aged'
+                end as Age_cat
+from customer;
 
-
-
+--ejemplo 2:
+Select customer_id,customer_name, age , Case 
+                when age <30 then 'Young'
+                when age>60 then 'Senior Citizen'
+                else 'Middle Aged'
+                end as Age_cat
+from customer;
 
 
 
